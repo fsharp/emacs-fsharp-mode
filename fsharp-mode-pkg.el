98df2b4bdc75769973d5a3258c4feb3041a04eb4
Mark Laws <mdl@60hz.org>
2016-04-03 18:03:12 +0900

    Fix use of popup (e.g. "C-c C-t")

--- fsharp-mode-pkg.el
+++ fsharp-mode-pkg.el
@@ -4,6 +4,7 @@
   "F# mode for Emacs"
   '((company           "0.8.0")
     (company-quickhelp "1.2.0")
+    (popup             "0.5.3")
     (pos-tip           "0.4.5")
     (s                 "1.3.1")
     (dash              "1.1.0")))
