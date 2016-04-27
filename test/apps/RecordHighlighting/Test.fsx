2a3bce7d0788e45c8a6bdaf52f19cb0393077d84
Robin Neatherway <robin.neatherway@gmail.com>
2016-04-27 23:02:14 +0100

    Add a new test based on submitted issue

---
+++ test/apps/RecordHighlighting/Test.fsx
@@ -0,0 +1,28 @@
+type RecordTest1 = { something: int
+                     another: string }
+
+type RecordTest2 = { something :int; another :string }
+
+type RecordTest3 = { something : float; another: float; third :float; }
+
+type RecordTest4 = {
+    something: int
+    another: string }
+
+type RecordTest5 =
+    { something: int
+      another: string }
+
+type RecordTest6 =
+    {
+        something: int
+        another: string
+        third: Option<int>
+    }
+
+type RecordTest7 =
+    {
+    something: int
+    another: string
+    third: int option
+    }
