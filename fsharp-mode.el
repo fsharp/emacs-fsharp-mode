65c8cb1afd4c321a075b715982215f5ebced5cf3
Juergen Hoetzel <juergen@archlinux.org>
2016-05-22 13:14:02 +0200

    Don't clobber company-backends

--- fsharp-mode.el
+++ fsharp-mode.el
@@ -248,7 +248,7 @@
   (setq font-lock-defaults '(fsharp-font-lock-keywords))
   (setq syntax-propertize-function 'fsharp--syntax-propertize-function)
   ; Some reasonable defaults for company mode
-  (setq company-backends (list 'fsharp-ac/company-backend))
+  (add-to-list 'company-backends 'fsharp-ac/company-backend)
   (setq company-auto-complete 't)
   (setq company-auto-complete-chars ".")
   (setq company-idle-delay 0.03)
