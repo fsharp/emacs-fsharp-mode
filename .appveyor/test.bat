31b6540ce3f7e4d2af7711dba12a33a4677d4c3a
Juergen Hoetzel <juergen@archlinux.org>
2018-07-11 14:31:00 +0200

    Enable integration tests on Windows
    Fixes #12.

--- .appveyor/test.bat
+++
@@ -1,5 +0,0 @@
-set HOME=%~dp0\tmp
-
-emacs -l test\test-common.el --batch --eval "(progn (set-default-coding-systems 'utf-8) (load-packages))"
-
-emacs -l test\test-common.el --batch --eval "(progn (set-default-coding-systems 'utf-8) (run-fsharp-unit-tests))"
