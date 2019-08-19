3a965c4642739893053aefbd8bfdc9b4508d4050
Juergen Hoetzel <juergen@archlinux.org>
2019-08-19 09:15:11 +0200

    Add eglot support

---
+++ eglot-fsharp.el
@@ -0,0 +1,102 @@
+;;; eglot-fsharp.el --- Lua eglot integration                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Jürgen Hötzel
+
+;; Author: Jürgen Hötzel <juergen@archlinux.org>
+;; Package-Requires: ((eglot "1.4"))
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Lua eglot introduced
+
+;;; Code:
+
+(require 'eglot)
+(require 'gnutls)
+
+(defgroup eglot-fsharp nil
+  "LSP support for the F# Programming Language, using F# compiler service."
+  :link '(url-link "https://github.com/fsharp/FsAutoComplete")
+  :group 'eglot)
+
+(defcustom eglot-fsharp-server-install-dir
+  (locate-user-emacs-file "FsAutoComplete/")
+  "Install directory for FsAutoComplete."
+  :group 'eglot-fsharp
+  :risky t
+  :type 'directory)
+
+(defcustom eglot-fsharp-server-version "0.38.1"
+  "FsAutoComplete version."
+  :group 'eglot-fsharp
+  :risky t
+  :type 'string)
+
+(defcustom eglot-fsharp-server-runtime
+  (if (executable-find "dotnet")
+      'net-core
+    'net-framework)
+  "The .NET runtime to use."
+  :group 'eglot-fsharp
+  :type '(choice (const :tag "Use .Net Core" 'net-core)
+                 (const :tag "Use .Net Framework" 'net-framework)))
+
+(defun eglot-fsharp--path-to-server ()
+  "Return FsAutoComplete path."
+  (file-truename (concat eglot-fsharp-server-install-dir
+			 (if (eq eglot-fsharp-server-runtime 'net-core)
+			     "netcore/fsautocomplete.dll"
+			   "netframework/fsautocomplete.exe"))))
+
+(defun eglot-fsharp--maybe-install ()
+  "Downloads F# compiler service, and install in `eglot-fsharp-server-install-dir'."
+  (make-directory (file-name-directory (eglot-fsharp--path-to-server)) t)
+  (let* ((url (format "https://ci.appveyor.com/api/projects/fsautocomplete/fsautocomplete/artifacts/bin/pkgs/fsautocomplete%szip?branch=master"
+		      (if (eq eglot-fsharp-server-runtime 'net-core)
+			  ".netcore."
+			".")))
+	 (exe (eglot-fsharp--path-to-server))
+	 (zip (concat (file-name-directory exe) (file-name-nondirectory url)))
+	 (gnutls-algorithm-priority
+	  (if (and (not gnutls-algorithm-priority)
+			  (boundp 'libgnutls-version)
+			  (>= libgnutls-version 30603)
+			  (version<= emacs-version "26.2"))
+		     "NORMAL:-VERS-TLS1.3"
+		   gnutls-algorithm-priority)))
+    (unless (file-exists-p exe)
+      (url-copy-file url zip t)
+      ;; FIXME: Windows
+      (let ((default-directory (file-name-directory (eglot-fsharp--path-to-server))))
+	(unless (zerop (call-process "unzip" nil nil nil "-x" zip))
+	  (error "Failed to unzip %s" zip))))))
+
+(defun eglot-fsharp (interactive)
+  "Ensure FsAutoComplete is installed when called INTERACTIVE.
+Return `eglot' contact when Emmy is installed."
+  (unless (or (file-exists-p (eglot-fsharp--path-to-server)) (not interactive))
+    (eglot-fsharp--maybe-install))
+  (when (file-exists-p (eglot-fsharp--path-to-server))
+    `(,(if (eq eglot-fsharp-server-runtime 'net-core)
+	   "dotnet"
+	 ;; FIXME: Windows
+	 "mono") ,(eglot-fsharp--path-to-server) "--mode" "lsp" "--background-service-enabled")))
+
+(add-to-list 'eglot-server-programs `(fsharp-mode . eglot-fsharp))
+
+(provide 'eglot-fsharp)
+;;; eglot-fsharp.el ends herep
