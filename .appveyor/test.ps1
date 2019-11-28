e50359d67e92d3d3ccb2fefc6804cb2a466f7a06
MiyamotoAkira <yefo.akira@gmail.com>
2019-11-28 23:21:15 +0000

    Fixed mistype

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -5,7 +5,7 @@ emacs --version
 Invoke-Request -Uri https://raw.githubusercontent.com/joaotavora/eglot/master/eglot-tests.el
 dotnet restore $PSScriptRoot\..\test\Test1
 
-$location- Join-Path ($env:USERPROFILE) '\.cask\bin'
+$location= Join-Path ($env:USERPROFILE) '\.cask\bin'
 $env.Path += ";$location"
 cask install | Out-String
 cask update | Out-String
