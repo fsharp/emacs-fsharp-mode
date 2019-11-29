428672cd978e220e44b98f5b97ee70e92495eadb
MiyamotoAkira <yefo.akira@gmail.com>
2019-11-29 21:34:56 +0000

    Fixes around path
    The downloading of the file was missing the output file. Also,
    converted all file manipulation to use Join-Path and Split-Path

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,13 +1,13 @@
-$env:HOME='$PSScriptRoot\tmp'
+$env:HOME=(Join-Path $PSScriptRoot '\tmp')
 
 emacs --version
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/joaotavora/eglot/master/eglot-tests.el
-dotnet restore $PSScriptRoot\..\test\Test1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/joaotavora/eglot/master/eglot-tests.el -Outfile (Join-Path (Split-Path $PSScriptRoot -parent) "eglot-tests.el")
+dotnet restore (Join-Path (Split-Path $PSScriptRoot -parent) "test\Test1")
 
 $location= Join-Path ($env:USERPROFILE) '\.cask\bin'
 $env:Path += ";$location"
 cask install | Out-String
 cask update | Out-String
-Get-ChildItem $PSScriptRoot\..
-cask exec buttercup -L . -L $PSScriptRoot\..\test | Out-String
+
+cask exec buttercup -L . -L (Join-Path (Split-Path $PSScriptRoot -parent) "test") | Out-String
