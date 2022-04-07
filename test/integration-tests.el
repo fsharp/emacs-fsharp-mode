17866a01a24edf6728847a792a29e64e453fa3b8
Juergen Hoetzel <juergen@hoetzel.info>
2022-04-07 13:52:26 +0200

    New test: "Can be invoked"

--- test/integration-tests.el
+++ test/integration-tests.el
@@ -40,6 +40,10 @@
   (it "Can be installed"
     (eglot-fsharp--maybe-install)
     (expect (eglot-fsharp--installed-version) :to-equal (eglot-fsharp--latest-version)))
+  (it "Can be invoked"
+      ;; FIXME: Should use dotnet tool run
+      (expect (car (process-lines (eglot-fsharp--path-to-server) "--version"))
+	      :to-match (rx line-start "FsAutoComplete" (1+ space) (eval (eglot-fsharp--installed-version)))))
   (it "shows flymake errors"
     (with-current-buffer (eglot--find-file-noselect "test/Test1/Error.fs")
       (eglot--tests-connect 10)
